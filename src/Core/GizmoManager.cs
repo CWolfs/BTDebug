8569d20384e15b5688190fc0d0b4c35a34f970f5
Richard Griffiths <cwolfs@gmail.com>
2018-09-12 12:52:17 +0200

    Add a sphere for the lance spawn location as a temp visual

--- src/Core/GizmoManager.cs
+++ src/Core/GizmoManager.cs
@@ -15,9 +15,16 @@ namespace BTDebug {
     private static GizmoManager instance;
 
     public bool IsGizmoModeActive { get; private set; } = false;
+    
+    private GameObject encounterLayerParentGO;
 
     private Material regionMaterial;
     private List<GameObject> regionPointRepresentations = new List<GameObject>();
+
+    private GameObject activeEncounter;
+    private GameObject chunkPlayerLance;
+    private GameObject spawnerPlayerLance;
+    private List<GameObject> playerLanceSpawnPoints = new List<GameObject>();
 
     public static GizmoManager GetInstance() { 
       if (instance == null) instance = new GizmoManager();
@@ -30,10 +37,16 @@ namespace BTDebug {
     }
 
     public void ToggleGizmos() {
+      if (!encounterLayerParentGO) encounterLayerParentGO = GameObject.Find("EncounterLayerParent");
+
       if (IsGizmoModeActive) {
         DisableRegions();
+        DisableSpawns();
+        IsGizmoModeActive = false;
       } else {
         EnableRegions();
+        EnableSpawns();
+        IsGizmoModeActive = true;
       }
     }
 
@@ -54,6 +67,51 @@ namespace BTDebug {
       foreach (GameObject regionPointRepresentation in regionPointRepresentations) {
         MonoBehaviour.Destroy(regionPointRepresentation);
       }
+    }
+
+    private void EnableSpawns() {
+      if (!spawnerPlayerLance) {
+        chunkPlayerLance = GetActiveEncounterGameObject().transform.Find("Chunk_PlayerLance").gameObject;
+        spawnerPlayerLance = chunkPlayerLance.transform.Find("Spawner_PlayerLance").gameObject;
+        if (spawnerPlayerLance == null) { 
+          Main.Logger.LogError("[GizmoManager] No active encounters found");
+          return;
+        }
+      }
+
+      EnablePlayerLanceSpawn();
+    }
+
+    private void DisableSpawns() {
+
+    }
+
+    private void EnablePlayerLanceSpawn() {
+      GameObject placeholderPoint = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+      placeholderPoint.transform.parent = spawnerPlayerLance.transform;
+      placeholderPoint.transform.localPosition = Vector3.zero;
+
+      Vector3 position = spawnerPlayerLance.transform.position;
+      HexGrid hexGrid = ReflectionHelper.GetPrivateStaticField(typeof(WorldPointGameLogic), "hexGrid") as HexGrid;
+      Vector3 hexPosition = hexGrid.GetClosestPointOnGrid(position);
+      placeholderPoint.transform.position = hexPosition;
+
+      placeholderPoint.transform.localScale = new Vector3(100, 100, 100);
+      Material mat = placeholderPoint.GetComponent<Renderer>().material;
+      placeholderPoint.GetComponent<Renderer>().sharedMaterial = regionMaterial;
+    }
+
+    private GameObject GetActiveEncounterGameObject() {
+      if (activeEncounter) return activeEncounter;
+
+      foreach (Transform t in encounterLayerParentGO.transform) {
+        GameObject child = t.gameObject;
+        if (child.activeSelf) {
+          activeEncounter = t.gameObject;
+          return activeEncounter;
+        }
+      }
+      return null;
     }
   }
 }
