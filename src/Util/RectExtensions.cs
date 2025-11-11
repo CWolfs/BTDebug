efa5cf51d74df3b80e16d113fe3ed3be6adcfc36
Richard Griffiths <cwolfs@gmail.com>
2025-11-11 01:23:47 +0100

    Fixed encounter boundary position

--- src/Util/RectExtensions.cs
+++ src/Util/RectExtensions.cs
@@ -23,8 +23,8 @@ public static class RectExtensions {
   public static Rect GenerateUsableBoundary(this Rect boundaryRec) {
     float mapBorderSize = 50f;
     float mapSize = 2048f;
-    Rect edgeOfMapRec = new Rect(0, 0, mapSize - (mapBorderSize * 2f), mapSize - (mapBorderSize * 2f));
-    Rect testBounadaryRec = new Rect(boundaryRec.x + 974f, boundaryRec.y + 974f, boundaryRec.width, boundaryRec.height);
+    Rect edgeOfMapRec = new Rect(mapBorderSize, mapBorderSize, mapSize - (mapBorderSize * 2f), mapSize - (mapBorderSize * 2f));
+    Rect testBounadaryRec = new Rect(boundaryRec.x + 1024f, boundaryRec.y + 1024f, boundaryRec.width, boundaryRec.height);
     Rect boundaryIntersect;
     testBounadaryRec.Intersects(edgeOfMapRec, out boundaryIntersect);
     return boundaryIntersect;
